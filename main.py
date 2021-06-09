402616e709a9a231af1fbe2e982b33f731ed6749
jeremykao1 <>
2021-06-09 00:13:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+
+# for count in range(5):
+#   print('Hello World.')
+
+# print('Hello World.\n' * 5)
+
+# Remember you will have to bring some code over from Trinket.io, so I will see how you interact with the video to complete your submission appropriately.
+
